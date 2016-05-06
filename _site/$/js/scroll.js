592dc78a0d0d6d60a77eab768f313e1acc684a18
Jordan Dobson <jordandobson@gmail.com>
2016-05-05 19:24:25 -0700

    Updated download button and class Notebook link. Also scroll Javascript

--- _site/$/js/scroll.js
+++ _site/$/js/scroll.js
@@ -1,3 +1,44 @@
+/* Set this up with a closure */
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  var links = document.querySelectorAll("a.scroll"),
+  		count = links.length,
+			root  = /firefox|trident/i.test(navigator.userAgent) ? document.documentElement : document.body;
+			
+  function easeInOutCubic(t, b, c, d) {
+    if ((t/=d/2) < 1) return c/2*t*t*t + b
+    return c/2*((t-=2)*t*t + 2) + b
+  }
+
+  while (count--) 
+    links.item(count).addEventListener("click", function(e) {
+
+      var startTime,
+		      duration  = 900,
+      		startPos  = root.scrollTop,
+					endPos    = document.getElementById(/[^#]+$/.exec(this.href)[0]).getBoundingClientRect().top,
+					maxScroll = root.scrollHeight - window.innerHeight,
+					endScroll = startPos + endPos < maxScroll ? endPos : (maxScroll - startPos);
+					
+      function scroll(timestamp) {
+        startTime = startTime || timestamp
+        var elapsed  = timestamp - startTime,
+        		progress = easeInOutCubic(elapsed, startPos, endScroll, duration);
+        root.scrollTop = progress
+        elapsed < duration && requestAnimationFrame(scroll)
+      }   
+      requestAnimationFrame(scroll);
+      e.preventDefault();
+    })
+});
+
+
+
+
+
+
 /*
 document.addEventListener("DOMContentLoaded", () => {
   "use strict";
@@ -66,33 +107,3 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 */
 
-document.addEventListener("DOMContentLoaded", function() {
-  "use strict"
-
-  var links = document.querySelectorAll("a.scroll")
-  var i = links.length
-  var root = /firefox|trident/i.test(navigator.userAgent) ? document.documentElement : document.body
-  var easeInOutCubic = function(t, b, c, d) {
-    if ((t/=d/2) < 1) return c/2*t*t*t + b
-    return c/2*((t-=2)*t*t + 2) + b
-  }
-
-  while (i--) 
-    links.item(i).addEventListener("click", function(e) {
-      var startTime
-      var startPos = root.scrollTop
-      var endPos = document.getElementById(/[^#]+$/.exec(this.href)[0]).getBoundingClientRect().top
-      var maxScroll = root.scrollHeight - window.innerHeight
-      var scrollEndValue = startPos + endPos < maxScroll ? endPos : maxScroll - startPos
-      var duration = 900
-      var scroll = function(timestamp) {
-        startTime = startTime || timestamp
-        var elapsed = timestamp - startTime
-        var progress = easeInOutCubic(elapsed, startPos, scrollEndValue, duration)
-        root.scrollTop = progress
-        elapsed < duration && requestAnimationFrame(scroll)
-      }   
-      requestAnimationFrame(scroll)
-      e.preventDefault()
-    }) 
-})
